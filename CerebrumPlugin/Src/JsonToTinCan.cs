a92d50ae32bd4ae06b3c659404b713fdad13c5e2
adamburich <adamburich@gmail.com>
2025-04-15 11:07:57 -0400

    Shifting from xapi-wrapper to CerebrumLogger

---
+++ CerebrumPlugin/Src/JsonToTinCan.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using TinCan;
+
+namespace CerebrumLogger
+{
+    public class JsonToTinCan
+    {
+
+        /// <summary>
+        /// Constructs a TinCan.Statement object given a string of input json.
+        /// </summary>
+        /// <param name="jsonData"></param>
+        /// <returns>TinCan.Statement object</returns>
+        public static Statement toStatement(string jsonData)
+        {
+            JObject json = JObject.Parse(jsonData);
+            return new Statement(json);
+        }
+    }
+}
